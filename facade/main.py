181c59906b0e062345c89ed100db8538aa6bf69b
heoh <h0h6h2h5@gmail.com>
2021-05-05 09:19:56 +0000

    Refactor with facade pattern

--- facade/main.py
+++ facade/main.py
@@ -1,31 +1,17 @@
-from motor import Motor
+from car import Car
 
 def main():
     print("차를 제어합니다.")
+    
+    print("차의 부속품들을 어떻게 제어할지는 신경쓰지 않습니다.")
+    car = Car()
+    
+    car.go_straight()
+    car.turn_left()
+    car.turn_right()
+    car.go_backward()
+    car.stop()
 
-    print("차를 움직이려면 모터를 제어해야 합니다.")
-    left_motor = Motor("L Motor")
-    right_motor = Motor("R Motor")
-
-    # 전진
-    left_motor.set_speed(100)
-    right_motor.set_speed(100)
-
-    # 좌회전
-    left_motor.set_speed(-50)
-    right_motor.set_speed(50)
-
-    # 우회전
-    left_motor.set_speed(50)
-    right_motor.set_speed(-50)
-
-    # 후진
-    left_motor.set_speed(-100)
-    right_motor.set_speed(-100)
-
-    # 정지
-    left_motor.set_speed(0)
-    right_motor.set_speed(0)
 
 if __name__ == '__main__':
     main()
